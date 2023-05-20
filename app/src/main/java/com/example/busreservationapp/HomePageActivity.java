5163b5dac15025168ea4d9f8a9f83d4430c6459a
azisnurjaman <azisnurjaman03@gmail.com>
2023-05-21 00:37:30 +0700

    Merapihkan layout, menambahkan fitur date picker, menambahkan bottom navigation

--- app/src/main/java/com/example/busreservationapp/HomePageActivity.java
+++ app/src/main/java/com/example/busreservationapp/HomePageActivity.java
@@ -1,4 +1,50 @@
 package com.example.busreservationapp;
 
-public class HomePageActivity {
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class HomePageActivity extends AppCompatActivity {
+    final Calendar myCalendar= Calendar.getInstance();
+    EditText datePicker;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+
+        datePicker = findViewById(R.id.date);
+
+        DatePickerDialog.OnDateSetListener date =new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, month);
+                myCalendar.set(Calendar.DAY_OF_MONTH, day);
+                updateLabel();
+            }
+        };
+        datePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(HomePageActivity.this, date,
+                        myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH))
+                        .show();
+            }
+        });
+    }
+
+    private void updateLabel(){
+        String myFormat="dd/MM/yyyy";
+        SimpleDateFormat dateFormat = new SimpleDateFormat(myFormat, Locale.UK);
+        datePicker.setText(dateFormat.format(myCalendar.getTime()));
+    }
 }
