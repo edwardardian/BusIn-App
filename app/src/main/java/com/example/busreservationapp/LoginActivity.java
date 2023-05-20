d42f4d81a284b21c354fb67db1f06221681999b9
Edward Ardian Tayu <edwardtayu4@gmail.com>
2023-05-20 13:48:22 +0700

    bug fixing LoginActivity

--- app/src/main/java/com/example/busreservationapp/LoginActivity.java
+++ app/src/main/java/com/example/busreservationapp/LoginActivity.java
@@ -2,8 +2,10 @@ package com.example.busreservationapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,7 +14,6 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -30,6 +31,10 @@ public class LoginActivity extends AppCompatActivity {
 
     private Button btnGoogleLogin;
 
+    private ProgressBar progressBar;
+
+    private int RC_SIGN_IN = 5000;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,10 +42,11 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         btnGoogleLogin = findViewById(R.id.btnGoogleLogin);
+        progressBar = findViewById(R.id.progressBarLogin);
 
         btnGoogleLogin.setOnClickListener(v -> {
             googleSignIn();
-                });
+        });
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.web_client_id))
@@ -53,21 +59,24 @@ public class LoginActivity extends AppCompatActivity {
 
     private void googleSignIn() {
         Intent signInClient = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInClient, 2);
+        startActivityForResult(signInClient, RC_SIGN_IN);
+        progressBar.setVisibility(View.VISIBLE);
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-        if(requestCode == 2) {
+        if(requestCode == RC_SIGN_IN) {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             try {
                 GoogleSignInAccount account = task.getResult(ApiException.class);
-                Log.d("Google Sign In", "firebaseAuthWithGoogle" + account.getId());
+                account.getId();
+                progressBar.setVisibility(View.GONE);
+                Toast.makeText(LoginActivity.this, "Google sign in success!", Toast.LENGTH_SHORT).show();
                 firebaseAuthWithGoogle(account.getIdToken());
             } catch (ApiException e) {
-                Log.e("Google Sign In", "Google sign in failed", e);
+                progressBar.setVisibility(View.GONE);
+                Toast.makeText(LoginActivity.this, "Google sign in failed!", Toast.LENGTH_SHORT).show();
             }
         }
     }
@@ -79,19 +88,29 @@ public class LoginActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            Log.d("Google Sign In", "Login success!");
-                            FirebaseUser user = mAuth.getCurrentUser();
+                            progressBar.setVisibility(View.GONE);
+                            Toast.makeText(LoginActivity.this, "Login success!!", Toast.LENGTH_SHORT).show();
+                            FirebaseUser firebaseUser = mAuth.getCurrentUser();
+                            updateUI(firebaseUser);
                         } else {
-                            Log.d("Google Sign In", "Login failed!");
+                            progressBar.setVisibility(View.GONE);
+                            Toast.makeText(LoginActivity.this, "Google sign in failed!", Toast.LENGTH_SHORT).show();
+                            updateUI(null);
                         }
                     }
                 });
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        FirebaseUser firebaseUser = mAuth.getCurrentUser();
+    private void updateUI( FirebaseUser firebaseUser){
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
+        if(firebaseUser != null) {
+            String userName = account.getDisplayName();
+            String userEmail = account.getEmail();
+
+            startActivity(new Intent(LoginActivity.this, HomePageActivity.class));
+            finish();
+            Toast.makeText(LoginActivity.this, userName + userEmail, Toast.LENGTH_SHORT).show();
+        }
     }
 }
 
