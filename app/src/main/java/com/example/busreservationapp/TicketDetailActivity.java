1f06b498301704ce91e9aa4c7ef8a91f4c0b14cf
azisnurjaman <azisnurjaman03@gmail.com>
2023-06-06 14:17:33 +0700

    merapihkan layout

--- app/src/main/java/com/example/busreservationapp/TicketDetailActivity.java
+++ app/src/main/java/com/example/busreservationapp/TicketDetailActivity.java
@@ -1,4 +1,25 @@
 package com.example.busreservationapp;
 
-public class TicketDetailActivity {
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class TicketDetailActivity extends AppCompatActivity {
+    private FirebaseFirestore db;
+    private String departureCity, arrivalCity, departureHour, arrivalHour, departureTerminal, arrivalTerminal, busName, price,
+            tripTime, date, passengers, tripId;
+    private Trip trip;
+    private TextView name, phoneNumber, seats;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ticket_detail);
+
+
+    }
 }
